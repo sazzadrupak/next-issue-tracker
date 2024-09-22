f749952999436ba2103d432f3235344a931a2889
sazzadrupak <sazzad@oployeelabs.com>
2024-09-22 10:55:36 +0300

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,12 @@
+'use client';
+
+import classNames from 'classnames';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
 
 const NavBar = () => {
+  const currentPath = usePathname();
   const links = [
     { href: '/', label: 'Dashboard' },
     { href: '/issues', label: 'Issues' },
@@ -16,7 +21,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              'text-zinc-900': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
             href={link.href}
           >
             {link.label}
